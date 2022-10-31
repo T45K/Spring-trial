f8f3cf68cba411c83ca824d600484671cbafec09
T45K <tasktas9@gmail.com>
2022-10-31 22:23:29 +0900

    initial commit

---
+++ src/test/java/io/github/t45k/spring_trial/SpringTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.t45k.spring_trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
