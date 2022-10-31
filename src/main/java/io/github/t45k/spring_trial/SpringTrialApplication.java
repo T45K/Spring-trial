dbfaa6ff5767fe697cb79b4c07411418e196bdc2
T45K <tasktas9@gmail.com>
2022-10-31 22:25:16 +0900

    clean up

--- src/main/java/io/github/t45k/spring_trial/SpringTrialApplication.java
+++ src/main/java/io/github/t45k/spring_trial/SpringTrialApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTrialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTrialApplication.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringTrialApplication.class, args);
+    }
 }
